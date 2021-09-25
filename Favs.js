b5b4293dc3eaae4c4377faba408067f8d17f8582
Dam0123 <91379555+Dam0123@users.noreply.github.com>
2021-09-25 23:12:59 +0900

    Create Favs.js

---
+++ Favs.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+export default () => (
+
+    <View style={{flex:1, backgroundColor:"black"}}>
+      <Text>Favs</Text>
+    </View>
+);

21e91b25c0de59fa81ff1056b91bd61c82c88aad
Dam0123 <91379555+Dam0123@users.noreply.github.com>
2021-09-27 12:56:41 +0900

    Create MoviesPresenter.js

---
+++ MoviesPresenter.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
+import { ActivityIndicator, Dimensions } from "react-native";
+import Slide from "../../components/Movies/Slide";
+
+
+const Container = styled.View`
+flex: 1;
+background-color:black;
+justify-content: center;
+`
+
+
+export default ({loading, nowPlaying}) => (
+  <Container>
+      {nowPlaying == null ? (
+          <ActivityIndicator color="white" size="small" />
+      ) : (
+    <>
+        <Swiper controlsEnabled={false} loop timeout={3}>
+            {nowPlaying.map(movie => (
+                  <Slide
+                  key={movie.id}
+                  id={movie.id}
+                  title={movie.original_language}
+                  overview={movie.overview}
+                  votes={movie.vote_average}
+                  backgroundImage={movie.backdrop_path} 
+                  />
+            ))}
+        </Swiper>
+    </> 
+      )}  
+</Container>
+);
